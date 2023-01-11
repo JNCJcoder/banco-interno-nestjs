2261868038432c1147543f81f9dc5eb456a16cf1
JNCJcoder <jorgenettocjunior@gmail.com>
2023-01-11 20:55:55 -0300

    create Auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/users/user.entity';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) { }
+
+  async login(user: any) {
+    const payload = {
+      sub: user.id,
+      username: user.username,
+      accountId: user.accountId.id,
+    };
+
+    return { token: this.jwtService.sign(payload) };
+  }
+
+  async validateUser(username: string, password: string) {
+    let user: User;
+    try {
+      user = await this.usersService.findOneByUsername(username);
+    } catch (error) {
+      return null;
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return null;
+    }
+
+    return user;
+  }
+}
