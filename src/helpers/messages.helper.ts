491d4cc7269726f2b1263d3fe1f1410afa167800
JNCJcoder <jorgenettocjunior@gmail.com>
2023-01-11 20:54:49 -0300

    Create messages.helper.ts

---
+++ src/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+  PASSWORD_VALID:
+    'A senha precisa ter pelo menos 8 caracteres, um número e uma letra maiúscula.',
+  EMAIL_OR_PASSWORD_VALID: 'Email e/ou senha invalidos.',
+};
