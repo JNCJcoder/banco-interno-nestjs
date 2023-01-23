46b52caaf63dead2713c112ff54076df0eaaf794
JNCJcoder <jorgenettocjunior@gmail.com>
2023-01-23 16:32:10 -0300

    Auth com testes

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+const req = {
+  user: {
+    id: '0',
+    username: 'test',
+    accountId: {
+      id: '0'
+    }
+  }
+}
+
+const token = { token: 'test' };
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [{
+        provide: AuthService,
+        useValue: {
+          login: jest.fn().mockResolvedValue(token)
+        }
+      }]
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('login', () => {
+    it('should return a token', async () => {
+      const result = await authController.login(req);
+
+      expect(result).toEqual(token);
+    });
   });
 });
